495d3a2da359d5b55957d43c03583a112b5f5cf8
Caio Lucena <caiolucena.cc@gmail.com>
2018-06-21 06:45:25 -0300

    Ajustes finais de Segurança e autocomplete

--- erp/src/main/resources/static/javascripts/erp.js
+++ erp/src/main/resources/static/javascripts/erp.js
@@ -1,165 +1,183 @@
-var Erp = Erp ||{}
+var Erp = Erp || {}
 var $ = jQuery;
-Erp.MaskMoney = (function(){
-	function MaskMoney(){
-		this.decimal =  $('.js-decimal');
+Erp.MaskMoney = (function() {
+	function MaskMoney() {
+		this.decimal = $('.js-decimal');
 		this.plain = $('.js-plain');
 	}
-	MaskMoney.prototype.enable = function(){
-		this.decimal.maskMoney({thousands:'.', decimal:','}); //
-		this.plain.maskMoney({precision:0,thousands:"."});
+	MaskMoney.prototype.enable = function() {
+		this.decimal.maskMoney({
+			thousands : '.',
+			decimal : ','
+		}); //
+		this.plain.maskMoney({
+			precision : 0,
+			thousands : "."
+		});
 	}
 	return MaskMoney;
 }());
 
 Erp.MaskPhoneNumber = (function() {
-	
+
 	function MaskPhoneNumber() {
 		this.inputPhoneNumber = $('.js-phone-number');
 	}
-	
+
 	MaskPhoneNumber.prototype.enable = function() {
-		var maskBehavior = function (val) {
-		  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+		var maskBehavior = function(val) {
+			return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000'
+					: '(00) 0000-00009';
 		};
-		
+
 		var options = {
-		  onKeyPress: function(val, e, field, options) {
-		      field.mask(maskBehavior.apply({}, arguments), options);
-		    }
+			onKeyPress : function(val, e, field, options) {
+				field.mask(maskBehavior.apply({}, arguments), options);
+			}
 		};
-		
+
 		this.inputPhoneNumber.mask(maskBehavior, options);
 	}
-	
+
 	return MaskPhoneNumber;
-	
+
 }());
 
-Erp.MaskCpf = (function(){
+Erp.MaskCpf = (function() {
 
-	function MaskCpf(){
+	function MaskCpf() {
 		this.inputCpf = $('.js-cpf');
 	}
-	MaskCpf.prototype.enable = function(){
-		this.inputCpf.mask('000.000.000-00', {reverse: true});
-		
+	MaskCpf.prototype.enable = function() {
+		this.inputCpf.mask('000.000.000-00', {
+			reverse : true
+		});
+
 	}
-	
+
 	return MaskCpf;
 }());
-Erp.MaskData = (function(){
+Erp.MaskData = (function() {
 
-	function MaskCpf(){
+	function MaskCpf() {
 		this.inputData = $('.js-data');
 	}
-	MaskCpf.prototype.enable = function(){
-		this.inputData.mask('00/00/0000', {reverse: true});
-		
+	MaskCpf.prototype.enable = function() {
+		this.inputData.mask('00/00/0000', {
+			reverse : true
+		});
+
 	}
-	
+
 	return MaskCpf;
 }());
 
-Erp.MaskCfop = (function(){
+Erp.MaskCfop = (function() {
 
-	function MaskCfop(){
+	function MaskCfop() {
 		this.inputCfop = $('.js-cfop');
 	}
-	MaskCfop.prototype.enable = function(){
-		this.inputCfop.mask('0.000', {reverse: true});
-		
+	MaskCfop.prototype.enable = function() {
+		this.inputCfop.mask('0.000', {
+			reverse : true
+		});
+
 	}
-	
+
 	return MaskCfop;
 }());
 
-Erp.MaskNcm = (function(){
+Erp.MaskNcm = (function() {
 
-	function MaskNcm(){
+	function MaskNcm() {
 		this.inputNcm = $('.js-ncm');
 	}
-	MaskNcm.prototype.enable = function(){
-		this.inputNcm.mask('00000000', {reverse: true});
-		
+	MaskNcm.prototype.enable = function() {
+		this.inputNcm.mask('00000000', {
+			reverse : true
+		});
+
 	}
-	
+
 	return MaskNcm;
 }());
 
-Erp.MaskCest = (function(){
+Erp.MaskCest = (function() {
 
-	function MaskCest(){
+	function MaskCest() {
 		this.inputCest = $('.js-cest');
 	}
-	MaskCest.prototype.enable = function(){
-		this.inputCest.mask('0000000', {reverse: true});
-		
+	MaskCest.prototype.enable = function() {
+		this.inputCest.mask('0000000', {
+			reverse : true
+		});
+
 	}
-	
+
 	return MaskCest;
 }());
 
-Erp.MaskIcms = (function(){
+Erp.MaskIcms = (function() {
 
-	function MaskIcms(){
+	function MaskIcms() {
 		this.inputIcms = $('.js-icms');
 	}
-	MaskIcms.prototype.enable = function(){
-		this.inputIcms.mask('000', {reverse: true});
-		
+	MaskIcms.prototype.enable = function() {
+		this.inputIcms.mask('000', {
+			reverse : true
+		});
+
 	}
-	
+
 	return MaskIcms;
 }());
 
-Erp.MaskCnpj = (function(){
+Erp.MaskCnpj = (function() {
 
-	function MaskCnpj(){
+	function MaskCnpj() {
 		this.inputCnpj = $('.js-cnpj');
 	}
-	MaskCnpj.prototype.enable = function(){
-		this.inputCnpj.mask('000.000.000/0000-00', {reverse: true});
-		
+	MaskCnpj.prototype.enable = function() {
+		this.inputCnpj.mask('000.000.000/0000-00', {
+			reverse : true
+		});
+
 	}
-	
+
 	return MaskCnpj;
 }());
 
 Erp.MaskCep = (function() {
-	
+
 	function MaskCep() {
 		this.inputCep = $('.js-cep');
 	}
-	
+
 	MaskCep.prototype.enable = function() {
 		this.inputCep.mask('00.000-000');
 	}
-	
+
 	return MaskCep;
-	
-	
-	
+
 }());
 
-
 Erp.MaskDate = (function() {
-	
+
 	function MaskDate() {
 		this.inputDate = $('.js-date');
 	}
-	
+
 	MaskDate.prototype.enable = function() {
 		this.inputDate.mask('00/00/0000');
 		this.inputDate.datepicker({
-			//orientation: 'bottom',
-			language: 'pt-BR',
-			autoclose: true
+			// orientation: 'bottom',
+			language : 'pt-BR',
+			autoclose : true
 		});
 	}
-	
+
 	return MaskDate;
-	
+
 }());
 
 numeral.language('pt-br');
@@ -171,38 +189,54 @@ Erp.formatarMoeda = function(valor) {
 Erp.recuperarValor = function(valorFormatado) {
 	return numeral().unformat(valorFormatado);
 }
+Erp.MaskHour = (function() {
 
+	function MaskHour() {
+		this.inputHour = $('.js-hour');
+	}
+	MaskHour.prototype.enable = function() {
+		this.inputHour.mask('00:00', {
+			reverse : true
+		});
 
-$(function(){
+	}
 
-		var maskMoney = new Erp.MaskMoney();
-		maskMoney.enable();
-		
-		var maskPhoneNumber = new Erp.MaskPhoneNumber();
-		maskPhoneNumber.enable();
-		
-		var maskCpf = new Erp.MaskCpf();
-		maskCpf.enable();
-		
-		var maskCep = new Erp.MaskCep();
-		maskCep.enable();
-		
-		var maskCnpj = new Erp.MaskCnpj();
-		maskCnpj.enable();
-		
-		var maskCfop = new Erp.MaskCfop();
-		maskCfop.enable();
-		
-		var maskNcm = new Erp.MaskNcm();
-		maskNcm.enable();
-		
-		var maskCest = new Erp.MaskCest();
-		maskCest.enable();
-		
-		var maskIcms = new Erp.MaskIcms();
-		maskIcms.enable();
+	return MaskHour;
+}());
 
-		var maskDate = new Erp.MaskDate();
-		maskDate.enable();
-		
+$(function() {
+
+	var maskMoney = new Erp.MaskMoney();
+	maskMoney.enable();
+
+	var maskPhoneNumber = new Erp.MaskPhoneNumber();
+	maskPhoneNumber.enable();
+
+	var maskCpf = new Erp.MaskCpf();
+	maskCpf.enable();
+
+	var maskCep = new Erp.MaskCep();
+	maskCep.enable();
+
+	var maskCnpj = new Erp.MaskCnpj();
+	maskCnpj.enable();
+
+	var maskCfop = new Erp.MaskCfop();
+	maskCfop.enable();
+
+	var maskNcm = new Erp.MaskNcm();
+	maskNcm.enable();
+
+	var maskCest = new Erp.MaskCest();
+	maskCest.enable();
+
+	var maskIcms = new Erp.MaskIcms();
+	maskIcms.enable();
+
+	var maskDate = new Erp.MaskDate();
+	maskDate.enable();
+	
+	var maskHour = new Erp.MaskHour();
+	maskHour.enable();
+
 });
